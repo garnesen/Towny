dd5a719599990519a8e83b17a5906674fa3f360f
garnesen <garnesen@vt.edu>
2016-04-26 11:50:20 -0400

    Data structure changes

--- src/com/palmergames/bukkit/towny/war/eventwar/WarZoneData.java
+++ src/com/palmergames/bukkit/towny/war/eventwar/WarZoneData.java
@@ -1,6 +1,9 @@
 package com.palmergames.bukkit.towny.war.eventwar;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.bukkit.entity.Player;
 
@@ -11,80 +14,94 @@ import com.palmergames.bukkit.towny.object.TownyUniverse;
 
 public class WarZoneData {
 
-	private HashSet<Player> attackers;
+	private HashMap<Town, HashSet<Player>> attackerTowns;
+	private LinkedHashSet<Player> attackerQueue;
 	private HashSet<Player> defenders;
-	private HashSet<Town> attackerTowns;
 	private HashSet<Player> allPlayers;
 	
-	public WarZoneData () {
-		attackers = new HashSet<Player>();
+	public WarZoneData() {
+		attackerTowns = new HashMap<Town, HashSet<Player>>();
+		attackerQueue = new LinkedHashSet<Player>();
 		defenders = new HashSet<Player>();
-		attackerTowns = new HashSet<Town>();
 		allPlayers = new HashSet<Player>();
 	}
 
-	public int getHealthChange () {
-		//check config
-		return defenders.size() - attackers.size();
+	public int getHealthChange() {
+		return defenders.size() - attackerQueue.size();
 	}
 	
-	public void addAttacker (Player p) throws NotRegisteredException {
-		Resident resident = TownyUniverse.getDataSource().getResident(p.getName());
-		attackerTowns.add(resident.getTown());
+	public void addAttacker(Player p) throws NotRegisteredException {
+		Town town = getResident(p).getTown();
+		if (attackerTowns.containsKey(town)) {
+			attackerTowns.get(town).add(p);
+		} else {
+			HashSet<Player> playerAttackers = new HashSet<Player>();
+			playerAttackers.add(p);
+			attackerTowns.put(town, playerAttackers);
+		}
 		allPlayers.add(p);
-		attackers.add(p);
+		attackerQueue.add(p);
 	}
 	
-	public void addDefender (Player p) {
+	public void addDefender(Player p) {
 		allPlayers.add(p);
 		defenders.add(p);
 	}
 	
+	public void removeDefender(Player p) {
+		allPlayers.remove(p);
+		defenders.remove(p);
+	}
+	
+	public void removeAttacker(Player p) throws NotRegisteredException {
+		allPlayers.remove(p);
+		Town attackerTown = getResident(p).getTown();
+		HashSet<Player> attackerPlayers = attackerTowns.get(attackerTown);
+		if (attackerPlayers != null) {
+			attackerPlayers.remove(p);
+			if (attackerPlayers.size() == 0) {
+				attackerTowns.remove(attackerTown);
+			}
+		}
+		attackerQueue.remove(p);
+	}
+	
 	public boolean hasAttackers() {
-		return !attackers.isEmpty();
+		return attackerQueue.size() != defenders.size();
 	}
 	
 	public boolean hasDefenders() {
 		return !defenders.isEmpty();
 	}
 	
-	public HashSet<Player> getAttackers() {
-		return attackers;
-	}
-	
 	public HashSet<Player> getDefenders() {
 		return defenders;
 	}
 	
-	public HashSet<Town> getAttackerTowns() {
-		return attackerTowns;
+	public Set<Town> getAttackerTowns() {
+		return attackerTowns.keySet();
 	}
 	
-	public Player getRandomAttacker() {
-		int index = (int)(Math.random() * attackers.size());
-		int curIndex = 0;
-		for (Player p : attackers) {
-			if (curIndex == index)
-				return p;
-			curIndex++;
+	public Player getLongestStandingAttacker() {
+		if (!attackerQueue.isEmpty()) {
+			return attackerQueue.iterator().next();
 		}
-		//No players in the list
 		return null;
 	}
 	
 	public Player getRandomDefender() {
-		int index = (int)(Math.random() * defenders.size());
-		int curIndex = 0;
-		for (Player p : defenders) {
-			if (curIndex == index)
-				return p;
-			curIndex++;
+		// Not truly random, gets the first player in the set
+		if (!defenders.isEmpty()) {
+			return defenders.iterator().next();
 		}
-		//No players in the list
 		return null;
 	}
 	
 	public HashSet<Player> getAllPlayers() {
 		return allPlayers;
+	}
+	
+	private Resident getResident(Player p) throws NotRegisteredException {
+		return TownyUniverse.getDataSource().getResident(p.getName());
 	}
 }
